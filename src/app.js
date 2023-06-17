48d071aacb5211a24084d7bb3fa869b201dedca2
Jose David Caro Cantor <43614556+josecaro02@users.noreply.github.com>
2023-06-17 02:13:36 +0000

    Create new variables and add 5

--- src/app.js
+++ src/app.js
@@ -8,4 +8,7 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let total = 0;
+  total += 5;
+  total = total + 5;
 };
